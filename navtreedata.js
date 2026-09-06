0e033661fc631f8b3ca3bc3386a565475aeaa8c6
Kirbyrawr <devkirbyrawr@gmail.com>
2026-09-06 18:20:56 +0200

    Docs for deki-rtc from deki-editor 9500697

--- navtreedata.js
+++ navtreedata.js
@@ -26,8 +26,7 @@ var NAVTREE =
 [
   [ "deki-rtc", "index.html", [
     [ "Components", "index.html#autotoc_md0", null ],
-    [ "Feature flags", "index.html#autotoc_md1", null ],
-    [ "Component reference", "index.html#autotoc_md2", [
+    [ "Component reference", "index.html#autotoc_md1", [
       [ "DS3231RTCComponent", "index.html#c-ds3231rtccomponent", null ],
       [ "SystemClockRTCComponent", "index.html#c-systemclockrtccomponent", null ]
     ] ]
--- navtreeindex0.js
+++ navtreeindex0.js
@@ -3,8 +3,7 @@ var NAVTREEINDEX0 =
 "index.html":[],
 "index.html#autotoc_md0":[0],
 "index.html#autotoc_md1":[1],
-"index.html#autotoc_md2":[2],
-"index.html#c-ds3231rtccomponent":[2,0],
-"index.html#c-systemclockrtccomponent":[2,1],
+"index.html#c-ds3231rtccomponent":[1,0],
+"index.html#c-systemclockrtccomponent":[1,1],
 "pages.html":[]
 };
